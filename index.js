ca96a121f6c607cdd77abd962035f8d506d30cb7
Priviledge <privilledgeblack8@gmail.com>
2024-11-07 23:08:14 +0200

    added computer plat function and some suggestions which are commented below or next to the code im suggesting a change for

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
+//Function to randomly choose rock, paper, or scissors for the computer
+function computerPlay() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}
 
-
-
-
-
-
-var playerSelection="Rock";
+var playerSelection="";
 //single play function
 
 var playRound = function(playerSelection, computerChoice){
    playerSelection = playerSelection.toLowerCase();
-   let result = "";
+  let result = "";//computerChoice = computerChoice.toLowerCase();
 
    if(playerSelection === computerChoice){
        alert(`Its a tie ${playerSelection} : ${computerChoice}`)
@@ -18,6 +19,9 @@ var playRound = function(playerSelection, computerChoice){
            case "rock":
                (computerChoice === "scissors") ? alert("You Win!! Rock beats Paper") : alert("You Lose : ( Paper beats Rock");
                break;
+               /* case 'rock':
+            return (computerChoice === 'scissors') ? "You win! Rock beats scissors." : "You lose! Paper beats rock.";
+     */
            case "paper":
                 (computerChoice === "rock") ? alert("You Win!! Paper beats Rock") : alert("You Lose : ( Scissors beat Paper");
                 break;
@@ -27,5 +31,6 @@ var playRound = function(playerSelection, computerChoice){
        }
    }
    //replace alert with result??
+   //rather return the whole thing ( i left an example below the first break)
 };
 playRound(playerSelection, computerChoice);
