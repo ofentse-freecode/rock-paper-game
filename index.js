dd590f89a1b1c693d742fc33797f1bdbf8406f26
dania <alsmandania826@gmail.com>
2024-11-09 01:30:24 +0300

    write game and fix playround

--- index.js
+++ index.js
@@ -12,30 +12,65 @@ function computerPlay() {
  computerPlay()*/
 
 var playerSelection="";
-//single play function. call in game input.
 
-var playRound = function(playerSelection, computerChoice){
-   playerSelection = playerSelection.toLowerCase();
-  let result = "";
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
 
-   if(playerSelection === computerChoice){
-       alert(`Its a tie ${playerSelection} : ${computerChoice}`)
-   }else{
-       switch(playerSelection){
-           case "rock":
-               (computerChoice === "scissors") ? result = "You Win!!" : result = "You Lose";
-               break;
-           case "paper":
-                (computerChoice === "rock") ? result = "You Win!!" : result = "You Lose";
-                break;
-            case "scissors":
-               (computerChoice === "paper") ? result = "You Win!!"  : result = "You Lose";
-               break;
-       }
-   }
-   return result
+    if (playerSelection === computerSelection) {
+        return `Its a tie ${playerSelection} : ${computerChoice}`;
+    } else if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "scissors" && computerSelection === "paper") ||
+        (playerSelection === "paper" && computerSelection === "rock")
+    ) {
+        return "win";
+    } else {
+        return "lose";
+    }
+}
 
-};
-playRound(playerSelection, computerChoice);
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
 
+    console.log("Welcome to the Rock Paper Scissors Game!");
+    console.log("First to win 5 rounds is the winner. Good luck!");
 
+    for (let round = 1; round <= 5; round++) {
+        
+        console.log(`\nRound ${round}:`);
+        
+        let playerSelection = prompt("Enter Rock, Paper, or Scissors:");
+        let computerSelection = computerPlay();
+        
+        let result = playRound(playerSelection, computerSelection);
+
+        if (result === "win") {
+            playerScore++;
+            console.log(`You Win! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}`);
+        } else if (result === "lose") {
+            computerScore++;
+            console.log(`You Lose! ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}`);
+        } else {
+            console.log(`It's a tie! Both chose ${capitalize(playerSelection)}`);
+        }
+        
+        console.log(`Score: You ${playerScore} - ${computerScore} Computer`);
+    }
+
+    console.log("\nGame Over!");
+    if (playerScore > computerScore) {
+        console.log("Congratulations! You won the game!");
+    } else if (computerScore > playerScore) {
+        console.log("Sorry, you lost the game. Better luck next time!");
+    } else {
+        console.log("It's a draw! No one wins this time.");
+    }
+}
+
+function capitalize(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
+game();
