096bf811b21999434106a666579d429682b678ef
Priviledge <privilledgeblack8@gmail.com>
2024-11-11 11:31:46 +0200

    made comments on what to change to improve game

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function playRound(playerSelection, computerSelection) {
         return `You Win! ${(playerSelection)} beats ${(computerSelection)}`;
 
     } else if(playerSelection == undefined || !["rock", "paper", "scissors"].includes(playerSelection)){
-        return 'Invalid input! Please enter rock or paper or scissors !!'
+        return 'Invalid input! Please enter rock or paper or scissors !!'// this warnig has to appear in the alert popup
     }
     else{
 
@@ -41,15 +41,15 @@ function playRound(playerSelection, computerSelection) {
 
 function game() {
     alert("Hello guys, I’ve hacked this browser, Welcome to the game of ROCK, PAPER or SCISSORS :)");
-
+      //Hello Humans I have taken control of this code and the only way to defeat me is by accepting my challage to play the rock paper scissors game, the rules are simple whoever wins the game owns the code forever HAHAHAHAHA
     let playerScore = 0;
     let computerScore = 0;
 
     for (let i = 1; i <= 5; i++) {
 
         const playerSelection = getPlayerSelection();
-        if (playerSelection === "exit") {
-            console.log("Game canceled by player!");
+        if (playerSelection === "exit") { 
+            console.log("Game canceled by player!");//instead of console.log(we can alert(aaah You leaving so soon!!! weakingly Human you could not comprehent my power as an AI thats why you coward in fear and decided to leave the game . HHAHAHAHAHA im so awesome))
             return; 
         }
 
@@ -58,7 +58,7 @@ function game() {
         console.log(`Round ${i}: ${result}`);
 
         if (result.includes("Win")) {
-            playerScore++;
+            playerScore++; // if player wins congratulate them with an alert(AAAAAHHHHH !!! you have defeated me, i should have not under estimate your knowledge but know i will return HAHAHAHAHAH)
         } else if (result.includes("Lose")) {
             computerScore++;
         }
